d6c7c379aa3bb9daf9d75ee96ff852e40a21ea14
Danilo Silva <danilo.silva@itenabler.com.br>
2021-05-05 11:22:18 -0300

    Cap 7 e Conclusao

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,7 @@
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -11,10 +14,24 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService, private router: Router){}
+
   transferir() {
     console.log('Solicitada nova transferência');
-    const valorEmitir = { valor: this.valor, destino: this.destino };
-    this.aoTransferir.emit(valorEmitir);
+
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    );
 
     this.limparCampos();
   }
